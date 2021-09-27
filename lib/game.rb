a412057f9b004e9d68cfd6656964015c9e786c16
Alek <alekseii.sm@gmail.com>
2021-09-27 18:53:27 +0300

    add all backend functionality

--- lib/game.rb
+++ lib/game.rb
@@ -6,7 +6,9 @@
 # this functionality, but anyway.
 class Game
   @state = 8 # last is 0
-  @word = []
+  @word_letters = []
+  @guesses = []
+  @word = ''
 
   class << self
     attr_reader :state
@@ -15,18 +17,32 @@ class Game
   # rules are stable, this is the last version
   def self.start(word)
     @state = 8
-    @word = word.split('')
+    @word = word
+    @word_letters = word.split('').uniq # bad tr
+    @guesses = []
   end
 
   def self.move(letter)
-    @state -= 1 unless @word.any? letter
+    @state -= 1 unless @word_letters.any? letter
+    @word_letters -= [letter]
+    @guesses << letter
   end
 
   def self.won?
-    false
+    @word_letters.empty?
   end
 
   def self.lost?
     @state.zero?
+  end
+
+  def self.guesses
+    @guesses.clone
+  end
+
+  def self.hint
+    @word.split('').map do |letter|
+      @guesses.any?(letter) ? letter : '_'
+    end.join
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,7 +5,7 @@ require 'game'
 describe Game do
   before :each do
     @word = 'hangman'
-    @right_letters = 'hangman'.split('')
+    @right_letters = 'hangman'.split('').uniq
     @wrong_letters = ('a'..'z').to_a - @right_letters
 
     Game.start(@word)
@@ -29,5 +29,26 @@ describe Game do
     expect(Game.state).to eq 8
   end
 
-  it 'player wins after pre-definded number of right moves'
+  it 'player wins after pre-definded number of right moves' do
+    @right_letters.each do |letter|
+      Game.move(letter)
+    end
+    expect(Game.state).to eq 8
+    expect(Game.won?).to be true
+    expect(Game.lost?).to be false
+  end
+
+  it 'keeps guesses' do
+    to_guess = @wrong_letters[0, 5] + @right_letters[0, 3]
+    to_guess.each { |letter| Game.move(letter) }
+    expect(Game.guesses).to match_array to_guess
+  end
+
+  it 'gives a hint' do
+    expect(Game.hint).to eq '_______'
+    Game.move('h')
+    expect(Game.hint).to eq 'h______'
+    Game.move('a')
+    expect(Game.hint).to eq 'ha___a_'
+  end
 end
