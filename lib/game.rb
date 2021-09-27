161b948dfe5fc647a584148aac01b99a5e0109c2
Alek <alekseii.sm@gmail.com>
2021-09-27 15:50:07 +0300

    add game state change. stub

--- lib/game.rb
+++ lib/game.rb
@@ -3,17 +3,22 @@
 # This is particualr type of the game,
 # suitable to manage hangman game.
 # Seems, hangrman "board" would be enough to cover
-# this functionality
+# this functionality, but anyway.
 class Game
   @state = 8 # last is 0
   @word = []
 
-  # rules are stable, this is the last version
-  def self.start(word)
-    @word = word.to_a
-  end
-
   class << self
     attr_reader :state
+  end
+
+  # rules are stable, this is the last version
+  def self.start(word)
+    @state = 8
+    @word = word.split('')
+  end
+
+  def self.move(letter)
+    @state -= 1
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,11 +5,19 @@ require 'game'
 describe Game do
   before :each do
     @word = 'hangman'
-    Game.start(word: @word)
+    @right_letters = 'hangman'.split('')
+    @wrong_letters = ('a'..'z').to_a - @right_letters
+
+    Game.start(@word)
   end
 
   it 'it changes its state consequently, if move is wrong,' do
     expect(Game.state).to eq 8
+    @wrong_letters[0, 8].each.with_index do |letter, i|
+      expect(Game.state).to eq 8 - i
+
+      Game.move(letter)
+    end
   end
   it 'at last state player loose'
   it 'if player makes a right move, state doesn`t change'
